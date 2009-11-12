64164e455c922799db4de81b39d2f31fbb79adf0
Whit Morriss <whit@openplans.org>
2009-11-11 19:39:23 -0500

    initial commit for ti layout and python package layout

---
+++ README.rst
@@ -0,0 +1,29 @@
+====
+loaf
+====
+
+
+Installation
+------------
+
+The easiest way to get loaf is if you have setuptools_ installed::
+
+	easy_install loaf
+
+Without setuptools, it's still pretty easy. Download the loaf.tgz file from 
+`loaf's Cheeseshop page`_, untar it and run::
+
+	python setup.py install
+
+.. _loaf's Cheeseshop page: http://pypi.python.org/pypi/loaf/
+.. _setuptools: http://peak.telecommunity.com/DevCenter/EasyInstall
+
+
+Help and development
+====================
+
+If you'd like to help out, you can fork the project
+at http://github.com/whitmo/loaf and report any bugs 
+at http://github.com/loaf/loaf/issues.
+
+
---
+++ docs/conf.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from pkg_resources import parse_version
+import pkginfo
+
+def _egg_info(path_to_egg='../'):
+    path_to_egg = os.path.join(
+        os.path.dirname(__file__), path_to_egg)
+    egg_info = pkginfo.Develop(path_to_egg)
+    release = egg_info.version
+    version = '%s.%s' % tuple(map(int, parse_version(release)[0:2]))
+    return egg_info.name, egg_info.author, version, release
+
+project, author, version, release = _egg_info()
+copyright = '2009, whit'
+
+# Extension
+extensions = [
+	 # -*-Extensions: -*-
+	]
+intersphinx_mapping = {'http://docs.python.org/': None}
+
+# Source
+master_doc = 'index'
+templates_path = ['_templates']
+source_suffix = '.rst'
+exclude_trees = ['_build']
+pygments_style = 'sphinx'
+
+# html build settings
+html_theme = 'default'
+html_static_path = ['_static']
+
+# htmlhelp settings
+htmlhelp_basename = '%sdoc' % project
+
+# latex build settings
+latex_documents = [
+    ('index', '%s.tex' % project, u'%s Documentation' % project,
+    author, 'manual'),
+]
