e48d805a24e58bb7c1e83120d283e4eadc7d39de
Whit Morriss <whit@openplans.org>
2009-12-11 17:07:47 -0600

    titanium application and some build stuff

--- Resources/index.js
+++ Resources/index.js
@@ -1,13 +1,29 @@
-window.onload = function()
-{
-	document.getElementById('version').innerHTML = Titanium.version;
-	document.getElementById('phone').innerHTML = Titanium.Platform.name + ' '+Titanium.Platform.version;
-	document.getElementById('b').onclick = function() { Titanium.Media.vibrate(); };
+function Bag(){};
 
-	Titanium.Gesture.addEventListener('shake',function(){
-		var alerty = Titanium.UI.createAlertDialog();
-		alerty.setTitle("Not stirred!");
-		alerty.show();
-	},false);
-	
+window.onload = function(){
+        //var tools = Titanium.UI.createWindow({url:'tools.html', title:'Drawing Tools'});
+        var appstate = new Bag();
+        var olmap = Titanium.UI.createWebView({url:'map.html', name:'olmap', appstate:appstate});
+        var tools = Titanium.UI.createWebView({url:'tools.html', name:'tools', appstate:appstate});
+        var compose = Titanium.UI.createButton({systemButton:Titanium.UI.iPhone.SystemButton.COMPOSE});
+        var done = Titanium.UI.createButton({systemButton:Titanium.UI.iPhone.SystemButton.DONE});
+	compose.addEventListener('click',
+            function(){
+                //
+	        Titanium.UI.currentWindow.setRightNavButton(done);
+                Titanium.UI.currentWindow.showView(tools, {animated:false});
+            }
+        );
+        done.addEventListener('click',
+                              function(){
+                                  Titanium.UI.currentWindow.showView(olmap, {animated:true});
+                                  Titanium.UI.currentWindow.setRightNavButton(compose);
+                              });
+	Titanium.UI.currentWindow.setRightNavButton(compose);
+        Titanium.UI.currentWindow.addView(olmap);
+        Titanium.UI.currentWindow.addView(tools);
+        Titanium.UI.currentWindow.showView(olmap, {animated:false});
+        Titanium.API.info(Titanium.UI.currentWindow + " is ready");
 };
+
+
